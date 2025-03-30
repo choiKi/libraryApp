aeb541991895cbb6a69fdcaf110011a9ce43c39e
choikihun <rlgns923@naver.com>
2025-03-30 18:18:55 +0900

    alter table name user->users

--- src/main/java/com/group/libraryapp/domain/user/loan/UserLoanHistoryRepository.java
+++ src/main/java/com/group/libraryapp/domain/user/loan/UserLoanHistoryRepository.java
@@ -6,6 +6,5 @@ import java.util.Optional;
 
 public interface UserLoanHistoryRepository extends JpaRepository<UserLoanHistory, Long> {
 
-    Optional<UserLoanHistory> findByUser_idAndBookName(long userId, String bookName);
     boolean existsByBookNameAndIsReturn(String bookName, boolean isReturn);
 }
